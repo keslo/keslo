88d3a9120db05155fcf2428ab87215380d3df888
re3derm.ru <44377905+re3derm@users.noreply.github.com>
2019-12-11 16:50:16 +0300

    tetris

---
+++ precache-manifest.02e1dec909366dc0e409f72555c736a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853f3cc617fb18d8620e44a5333aca32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58477e40195fc13dc269",
+    "url": "/static/css/main.3ec7f5fb.chunk.css"
+  },
+  {
+    "revision": "75429c94cfc5bcbbc8df",
+    "url": "/static/js/2.3f20a0a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3f20a0a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58477e40195fc13dc269",
+    "url": "/static/js/main.d71de66f.chunk.js"
+  },
+  {
+    "revision": "00b4f4d4a19c6ed002cb",
+    "url": "/static/js/runtime-main.e413a0e0.js"
+  }
+]);
